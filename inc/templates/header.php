f1a8df2108904772a275c7aab15d28724021de38
Joaquin Cognigni <joaquincognigni@MacBook-Pro-de-Joaquin.local>
2023-08-06 00:13:27 -0300

    MetaInf added

--- inc/templates/header.php
+++ inc/templates/header.php
@@ -4,11 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>UpTask</title>
+    <title>UpTask-Project Manager</title>
     <link rel="stylesheet" href="css/fontawesome-all.min.css">
     <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.0/normalize.css">
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700,900" rel="stylesheet">
     <link rel="stylesheet" href="css/sweetalert2.min.css">
     <link rel="stylesheet" href="css/style.css">
+    <!--MetaInf-->
+    <meta name="description" content="Project Manager">
+    <meta name="keywords" content="Projects, task, organization, managment">
+    <meta name="author" content="Joaquín Cognigni">
+    <meta name="copyright" content="Joaquín Cognigni 2023">
+    <!--Robots-->
+    <meta name="robots" content="index nofollow">
+    <link rel="icon" href="images/favicon.png" type="image/png">
+    <!--Facebook-->
+    <meta property="og:title" content="UpTask-Project Manager">
+    <meta property="og:description" content="This website will help you and your team to manage your projects and tasks">
+    <meta property="og:type" content="List">
+    <meta property="og:image" content="images/preview.png">
+    <meta property="og:url" content="https://uptask1233.000webhostapp.com/">
+    <!--Twitter-->
+    <meta property="twitter:title" content="Contact List">
+    <meta property="twitter:description" content="This website will help you and your team to manage your projects and tasks">
+    <meta property="twitter:type" content="List">
+    <meta property="twitter:image" content="images/preview.png">
+    <meta property="twitter:url" content="https://uptask1233.000webhostapp.com/">
 </head>
 <body class="<?php echo getPageName(); ?>">
